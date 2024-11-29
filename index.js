1911af35958c09515091e23f3616fed0945e7556
Huzsalih <salihhuza123@gmail.com>
2024-11-29 23:02:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function computeSum(a, b) {
+    if (a === b) {
+      return 3 * (a + b);
+    }
+    return a + b;
+  }
+  
+  // Example usage:
+  console.log(computeSum(5, 5)); // Output: 30
+  console.log(computeSum(5, 3)); // Output: 8
+  
